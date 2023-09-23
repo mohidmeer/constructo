d85003e3afca91ae71f92c7ee9b241c0c819da50
Mohid Meer <mohidfauji@gmail.com>
2023-09-23 18:46:13 +0500

    SEO

--- pages/index.js
+++ pages/index.js
@@ -20,11 +20,11 @@ export default function Home() {
                 {/* <meta property="og:image" content={ogimgurl} />
                  <meta property="og:image:width" content="400" />
                     <meta property="og:image:height" content="400" /> */}
-                <meta property="og:title" content={'Contact Us'} />
+                <meta property="og:title" content={'Constructo'} />
 
 
                 <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content={'Contact Us'} />
+                <meta name="twitter:title" content={'Constructo'} />
                 <meta name="twitter:description" content={"New Jersey Trusted Roofing Contractor. Protecting what matters the most. Whether you're in need of a full roof replacement or a roof repair, you can coun"} />
                 {/* <meta name="twitter:image" content={ogimgurl} /> */}
             </Head>
